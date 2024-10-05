14da621e232a084dac78dd631f962df4e42ea4b4
deancain1 <caindeanw@gmail.com>
2024-10-05 12:19:01 +0800

    Add files via upload

---
+++ Carl_Rental_System/User/AddCarControl.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace Carl_Rental_System.User
+{
+    partial class AddCarControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button4 = new System.Windows.Forms.Button();
+            this.browseBtn = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.uploadBtn = new System.Windows.Forms.Button();
+            this.carImageBox = new System.Windows.Forms.PictureBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.gas = new System.Windows.Forms.ComboBox();
+            this.transmission = new System.Windows.Forms.ComboBox();
+            this.seats = new System.Windows.Forms.ComboBox();
+            this.price = new System.Windows.Forms.TextBox();
+            this.carName = new System.Windows.Forms.TextBox();
+            this.plateNumber = new System.Windows.Forms.TextBox();
+            this.brand = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.carImageBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Crimson;
+            this.button4.Location = new System.Drawing.Point(672, 182);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(104, 49);
+            this.button4.TabIndex = 35;
+            this.button4.Text = "Delete";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // browseBtn
+            // 
+            this.browseBtn.BackColor = System.Drawing.Color.Azure;
+            this.browseBtn.Location = new System.Drawing.Point(672, 104);
+            this.browseBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.browseBtn.Name = "browseBtn";
+            this.browseBtn.Size = new System.Drawing.Size(104, 49);
+            this.browseBtn.TabIndex = 34;
+            this.browseBtn.Text = "Browse";
+            this.browseBtn.UseVisualStyleBackColor = false;
+            this.browseBtn.Click += new System.EventHandler(this.browseBtn_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button2.Location = new System.Drawing.Point(548, 182);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 49);
+            this.button2.TabIndex = 33;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // uploadBtn
+            // 
+            this.uploadBtn.BackColor = System.Drawing.Color.Chartreuse;
+            this.uploadBtn.Location = new System.Drawing.Point(548, 104);
+            this.uploadBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.uploadBtn.Name = "uploadBtn";
+            this.uploadBtn.Size = new System.Drawing.Size(104, 49);
+            this.uploadBtn.TabIndex = 32;
+            this.uploadBtn.Text = "Upload";
+            this.uploadBtn.UseVisualStyleBackColor = false;
+            this.uploadBtn.Click += new System.EventHandler(this.uploadBtn_Click);
+            // 
+            // carImageBox
+            // 
+            this.carImageBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.carImageBox.Location = new System.Drawing.Point(318, 104);
+            this.carImageBox.Margin = new System.Windows.Forms.Padding(2);
+            this.carImageBox.Name = "carImageBox";
+            this.carImageBox.Size = new System.Drawing.Size(204, 169);
+            this.carImageBox.TabIndex = 31;
+            this.carImageBox.TabStop = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(580, 410);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(108, 18);
+            this.label7.TabIndex = 30;
+            this.label7.Text = "Transmission";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(580, 351);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 18);
+            this.label6.TabIndex = 29;
+            this.label6.Text = "Gas";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(577, 286);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 18);
+            this.label5.TabIndex = 28;
+            this.label5.Text = "Seats";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(315, 433);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 18);
+            this.label3.TabIndex = 27;
+            this.label3.Text = "Type of Car";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(316, 351);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 18);
+            this.label2.TabIndex = 26;
+            this.label2.Text = "Plate Number";
+            // 
+            // gas
+            // 
+            this.gas.FormattingEnabled = true;
+            this.gas.Items.AddRange(new object[] {
+            "Premium",
+            "Unledead",
+            "Diesel"});
+            this.gas.Location = new System.Drawing.Point(580, 370);
+            this.gas.Margin = new System.Windows.Forms.Padding(2);
+            this.gas.Name = "gas";
+            this.gas.Size = new System.Drawing.Size(196, 21);
+            this.gas.TabIndex = 25;
+            // 
+            // transmission
+            // 
+            this.transmission.FormattingEnabled = true;
+            this.transmission.Items.AddRange(new object[] {
+            "Manual",
+            "Automatic"});
+            this.transmission.Location = new System.Drawing.Point(580, 430);
+            this.transmission.Margin = new System.Windows.Forms.Padding(2);
+            this.transmission.Name = "transmission";
+            this.transmission.Size = new System.Drawing.Size(196, 21);
+            this.transmission.TabIndex = 24;
+            // 
+            // seats
+            // 
+            this.seats.FormattingEnabled = true;
+            this.seats.Items.AddRange(new object[] {
+            "4 seats",
+            "5 seats",
+            "6 seats",
+            "8 seats"});
+            this.seats.Location = new System.Drawing.Point(580, 306);
+            this.seats.Margin = new System.Windows.Forms.Padding(2);
+            this.seats.Name = "seats";
+            this.seats.Size = new System.Drawing.Size(196, 21);
+            this.seats.TabIndex = 23;
+            // 
+            // price
+            // 
+            this.price.Location = new System.Drawing.Point(318, 525);
+            this.price.Margin = new System.Windows.Forms.Padding(2);
+            this.price.Multiline = true;
+            this.price.Name = "price";
+            this.price.Size = new System.Drawing.Size(216, 50);
+            this.price.TabIndex = 22;
+            // 
+            // carName
+            // 
+            this.carName.Location = new System.Drawing.Point(318, 453);
+            this.carName.Margin = new System.Windows.Forms.Padding(2);
+            this.carName.Multiline = true;
+            this.carName.Name = "carName";
+            this.carName.Size = new System.Drawing.Size(216, 50);
+            this.carName.TabIndex = 21;
+            // 
+            // plateNumber
+            // 
+            this.plateNumber.Location = new System.Drawing.Point(318, 371);
+            this.plateNumber.Margin = new System.Windows.Forms.Padding(2);
+            this.plateNumber.Multiline = true;
+            this.plateNumber.Name = "plateNumber";
+            this.plateNumber.Size = new System.Drawing.Size(216, 50);
+            this.plateNumber.TabIndex = 20;
+            // 
+            // brand
+            // 
+            this.brand.Location = new System.Drawing.Point(318, 295);
+            this.brand.Margin = new System.Windows.Forms.Padding(2);
+            this.brand.Multiline = true;
+            this.brand.Name = "brand";
+            this.brand.Size = new System.Drawing.Size(216, 50);
+            this.brand.TabIndex = 19;
+            // 
+            // AddCarControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.browseBtn);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.uploadBtn);
+            this.Controls.Add(this.carImageBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.gas);
+            this.Controls.Add(this.transmission);
+            this.Controls.Add(this.seats);
+            this.Controls.Add(this.price);
+            this.Controls.Add(this.carName);
+            this.Controls.Add(this.plateNumber);
+            this.Controls.Add(this.brand);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "AddCarControl";
+            this.Size = new System.Drawing.Size(1091, 678);
+            ((System.ComponentModel.ISupportInitialize)(this.carImageBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button browseBtn;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button uploadBtn;
+        private System.Windows.Forms.PictureBox carImageBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox gas;
+        private System.Windows.Forms.ComboBox transmission;
+        private System.Windows.Forms.ComboBox seats;
+        private System.Windows.Forms.TextBox price;
+        private System.Windows.Forms.TextBox carName;
+        private System.Windows.Forms.TextBox plateNumber;
+        private System.Windows.Forms.TextBox brand;
+    }
+}
